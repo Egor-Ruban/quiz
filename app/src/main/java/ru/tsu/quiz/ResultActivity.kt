04ac613a3777ee17dc3fa605a21fd54ad8b3255c
compucter <egor-ruban@mail.ru>
2020-03-02 02:43:14 +0700

    добавил иконку в гит. Интересно, зачем она нужна

--- app/src/main/java/ru/tsu/quiz/ResultActivity.kt
+++ app/src/main/java/ru/tsu/quiz/ResultActivity.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.result_activity.*
 
-class ResultActivity : AppCompatActivity() {
+class ResultActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.result_activity)
